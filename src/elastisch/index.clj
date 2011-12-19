854d3b3910891a56e5988d5c68d7ee827f62ea74
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-12-19 17:49:01 +0100

    Adding docs and to-dos.

--- src/elastisch/index.clj
+++ src/elastisch/index.clj
@@ -59,7 +59,9 @@
    :body mapping))
 
 (defn delete-mapping
+  "Allow to delete a mapping (type) along with its data. The REST endpoint is /{index}/{type} with DELETE method.
 
+   API Reference: http://www.elasticsearch.org/guide/reference/api/admin-indices-delete-mapping.html"
   [^String index-name ^String type-name]
   (rest/delete-req
    (urls/index-mapping index-name type-name)))
@@ -69,6 +71,9 @@
 ;;
 
 (defn update-settings
+  "Change specific index level settings in real time. 
+
+   API Reference: http://www.elasticsearch.org/guide/reference/api/admin-indices-update-settings.html"
   ([settings]
      (rest/json-put-req
       (urls/index-settings)
@@ -80,6 +85,10 @@
 
 
 (defn get-settings
+  "The get settings API allows to retrieve settings of index/indices
+
+   API Reference: http://www.elasticsearch.org/guide/reference/api/admin-indices-get-settings.html
+  "
   ([]
      (rest/json-get-req
       (urls/index-settings)))
@@ -92,10 +101,51 @@
 ;;
 
 (defn open
+  "Open index.
+
+  API Reference: http://www.elasticsearch.org/guide/reference/api/admin-indices-open-close.html
+  "
   [index-name]
   (rest/json-post-req (urls/index-open index-name)))
 
 (defn close
+  "Close index.
+
+  API Reference: http://www.elasticsearch.org/guide/reference/api/admin-indices-open-close.html
+  "
   [index-name]
   (rest/json-post-req (urls/index-close index-name)))
 
+;;
+;; Aliases
+;;
+
+;; defn get-aliases
+;; defn add-alias (+ with filter + with routing)
+;; defn add-aliases
+;; defn remove-alias
+;; defn rename-alias
+
+;;
+;; Analyze
+;;
+
+;; defn analyze
+
+;;
+;; Templates
+;; 
+
+;; defn get-template
+;; 
+;; defn delete-template
+
+
+;; defn refresh
+;; defn optimize
+;; defn flush
+;; defn snapshot
+;; defn stats
+;; defn status
+;; defn segments
+;; defn clear-cache
