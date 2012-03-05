978bfe822f5d1a3ee0fe0edbb0e465d7cef59d83
Michael S. Klishin <michael@novemberain.com>
2012-03-05 08:53:57 +0400

    1.0.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/elastisch "1.0.0-alpha1"
+(defproject clojurewerkz/elastisch "1.0.0-SNAPSHOT"
   :description "Clojure ElasticSearch Client"
   :repositories {
                  "clojure-releases" "http://build.clojure.org/releases"
