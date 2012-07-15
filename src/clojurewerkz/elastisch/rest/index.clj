26a382dc29270b51b117ceeb784e786c069fa2de
Michael S. Klishin <michael@novemberain.com>
2012-07-15 16:42:25 +0400

    Better documentation string

--- src/clojurewerkz/elastisch/rest/index.clj
+++ src/clojurewerkz/elastisch/rest/index.clj
@@ -8,11 +8,29 @@
 ;; create, delete, exists?
 ;;
 
-;; TODO: FIXME: mappings and settings can't be not specified right now.
 (defn create
   "The create index API allows to instantiate an index.
 
-   API Reference: http://www.elasticsearch.org/guide/reference/api/admin-indices-create-index.html"
+   Accepted options are :mappings and :settings. Both accept maps with the same structure as in the REST API.
+
+   Examples:
+
+    (clojurewerkz.elastisch.rest.index/create \"myapp_development\")
+    (clojurewerkz.elastisch.rest.index/create \"myapp_development\" :settings {\"number_of_shards\" 1})
+
+    (let [mapping-types {:person {:properties {:username   {:type \"string\" :store \"yes\"}
+                                               :first-name {:type \"string\" :store \"yes\"}
+                                               :last-name  {:type \"string\"}
+                                               :age        {:type \"integer\"}
+                                               :title      {:type \"string\" :analyzer \"snowball\"}
+                                               :planet     {:type \"string\"}
+                                               :biography  {:type \"string\" :analyzer \"snowball\" :term_vector \"with_positions_offsets\"}}}}]
+      (clojurewerkz.elastisch.rest.index/create \"myapp_development\" :mappings mapping-types))
+
+
+
+   Related ElasticSearch API Reference section:
+   http://www.elasticsearch.org/guide/reference/api/admin-indices-create-index.html"
   [index-name & {:keys [settings mappings]}]
   (rest/post (rest/index-url index-name) :body (if mappings
                                                  {:settings settings :mappings mappings}
