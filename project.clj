82b7f6c5f7c46c66dab5a85b40c61d3552fc9f9d
Michael Klishin <michael@defprotocol.org>
2013-03-01 20:41:28 +0400

    Test against Clojure 1.5 final, 1.6 master snapshot

--- project.clj
+++ project.clj
@@ -16,8 +16,10 @@
                        :dependencies [[clj-time            "0.4.4" :exclusions [org.clojure/clojure]]]
                        :plugins [[codox "0.6.1"]]}
                  :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-                 :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC17"]]}}
-  :aliases      {"all" ["with-profile" "dev:dev,1.3:dev,1.5"]}
+                 :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+                 :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+                 :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+  :aliases      {"all" ["with-profile" "dev:dev,1.3:dev,1.5:dev,1.6"]}
   :repositories {"sonatype"         {:url "http://oss.sonatype.org/content/repositories/releases"
                                      :snapshots false
                                      :releases {:checksum :fail :update :always}}
