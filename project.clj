95bb30cbab318399f9ce489f6094dc0f1e1065c6
Michael Klishin <michael@defprotocol.org>
2013-07-02 11:39:43 +0400

    1.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/elastisch "1.1.1-SNAPSHOT"
+(defproject clojurewerkz/elastisch "1.1.1"
   :url "http://clojureelasticsearch.info"
   :description "Minimalistic fully featured well documented Clojure ElasticSearch client"
   :license {:name "Eclipse Public License"}
