42898d6531e99cc18bd432aa076d5f5699122312
Michael Klishin <michael@clojurewerkz.org>
2013-11-27 21:57:27 +0400

    Ditto

--- test/clojurewerkz/elastisch/rest_api/search_scroll_test.clj
+++ test/clojurewerkz/elastisch/rest_api/search_scroll_test.clj
@@ -2,8 +2,9 @@
   (:require [clojurewerkz.elastisch.rest.document      :as doc]
             [clojurewerkz.elastisch.rest.index         :as idx]
             [clojurewerkz.elastisch.query         :as q]
-            [clojurewerkz.elastisch.fixtures :as fx])
-  (:use clojure.test clojurewerkz.elastisch.rest.response))
+            [clojurewerkz.elastisch.fixtures :as fx]
+            [clojurewerkz.elastisch.rest.response :refer :all]
+            [clojure.test :refer :all]))
 
 (use-fixtures :each fx/reset-indexes fx/prepopulate-articles-index)
 
