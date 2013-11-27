4d75033487f28df1d2b345535a40c612b7b2cebd
Michael Klishin <michael@clojurewerkz.org>
2013-11-27 21:57:36 +0400

    Ditto

--- test/clojurewerkz/elastisch/rest_api/multi_test.clj
+++ test/clojurewerkz/elastisch/rest_api/multi_test.clj
@@ -4,8 +4,9 @@
             [clojurewerkz.elastisch.rest          :as rest]
             [clojurewerkz.elastisch.rest.multi    :as multi]
             [clojurewerkz.elastisch.query         :as q]
-            [clojurewerkz.elastisch.fixtures      :as fx])
-  (:use clojure.test clojurewerkz.elastisch.rest.response))
+            [clojurewerkz.elastisch.fixtures      :as fx]
+            [clojurewerkz.elastisch.rest.response :refer :all]
+            [clojure.test :refer :all]))
 
 (use-fixtures :each fx/reset-indexes fx/prepopulate-people-index fx/prepopulate-articles-index fx/prepopulate-tweets-index)
 
