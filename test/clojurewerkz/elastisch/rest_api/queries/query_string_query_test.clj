979ea9a25ef503112df290e1275ef5b23c38285e
Michael Klishin <michael@clojurewerkz.org>
2014-02-18 23:06:22 +0400

    Ditto

--- test/clojurewerkz/elastisch/rest_api/queries/query_string_query_test.clj
+++ test/clojurewerkz/elastisch/rest_api/queries/query_string_query_test.clj
@@ -1,3 +1,12 @@
+;; Copyright (c) 2011-2014 Michael S. Klishin, Alex Petrov, and the ClojureWerkz Team
+;;
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
 (ns clojurewerkz.elastisch.rest-api.queries.query-string-query-test
   (:refer-clojure :exclude [replace])
   (:require [clojurewerkz.elastisch.rest.document :as doc]
