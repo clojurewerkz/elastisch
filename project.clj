4317552bd9ad1b531ad744ffbea50ee25d8f4f2c
Michael Klishin <michael@clojurewerkz.org>
2014-03-27 18:23:37 +0400

    1.5 is an extra profile now

--- project.clj
+++ project.clj
@@ -19,9 +19,9 @@
                  ;; so we test for compatibility with it. MK.
                  :cljhttp076 {:dependencies [[clj-http "0.7.6"]]}
                  :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-                 :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+                 :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
                  :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
-  :aliases      {"all" ["with-profile" "dev:dev,1.4:dev,1.6:dev,cljhttp076:dev,1.6,cljhttp076"]}
+  :aliases      {"all" ["with-profile" "dev:dev,1.4:dev,1.5:dev,cljhttp076:dev,1.5,cljhttp076"]}
   :repositories {"sonatype"         {:url "http://oss.sonatype.org/content/repositories/releases"
                                      :snapshots false
                                      :releases {:checksum :fail :update :always}}
