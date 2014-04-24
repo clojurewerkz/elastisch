3195229d55f3b46cd86dd1a657fefabc09f7ddc4
Michael Klishin <michael@clojurewerkz.org>
2014-04-24 23:34:44 +0400

    Cosmetics

--- test/clojurewerkz/elastisch/rest_api/queries/flt_query_test.clj
+++ test/clojurewerkz/elastisch/rest_api/queries/flt_query_test.clj
@@ -11,17 +11,13 @@
   (:require [clojurewerkz.elastisch.rest.document :as doc]
             [clojurewerkz.elastisch.rest :as rest]
             [clojurewerkz.elastisch.rest.index    :as idx]
-            [clojurewerkz.elastisch.query         :as q]
+            [clojurewerkz.elastisch.query :as q]
             [clojurewerkz.elastisch.fixtures :as fx]
             [clojurewerkz.elastisch.rest.response :refer :all]
             [clojure.test :refer :all]))
 
 
 (use-fixtures :each fx/reset-indexes fx/prepopulate-articles-index)
-
-;;
-;; flt query
-;;
 
 (let [conn (rest/connect)]
   (deftest ^{:rest true :query true} test-basic-flt-query
