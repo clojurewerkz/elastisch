13f95edeb271a7efe67cb6a0253caab2f4d5d246
Michael Klishin <michael@clojurewerkz.org>
2014-04-26 02:12:14 +0400

    Ditto

--- src/clojurewerkz/elastisch/native/index.clj
+++ src/clojurewerkz/elastisch/native/index.clj
@@ -48,8 +48,8 @@
 
     (require '[clojurewerkz.elastisch.native.index :as idx])
 
-    (idx/create \"myapp_development\")
-    (idx/create \"myapp_development\" :settings {\"number_of_shards\" 1})
+    (idx/create conn \"myapp_development\")
+    (idx/create conn \"myapp_development\" :settings {\"number_of_shards\" 1})
 
     (let [mapping-types {:person {:properties {:username   {:type \"string\" :store \"yes\"}
                                                :first-name {:type \"string\" :store \"yes\"}
@@ -58,7 +58,7 @@
                                                :title      {:type \"string\" :analyzer \"snowball\"}
                                                :planet     {:type \"string\"}
                                                :biography  {:type \"string\" :analyzer \"snowball\" :term_vector \"with_positions_offsets\"}}}}]
-      (idx/create \"myapp_development\" :mappings mapping-types))
+      (idx/create conn \"myapp_development\" :mappings mapping-types))
 
    Related ElasticSearch API Reference section:
    http://www.elasticsearch.org/guide/reference/api/admin-indices-create-index.html"
