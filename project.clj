8f7a81526e5763f730b607bf54205d7424eec51c
Michael Klishin <michael@clojurewerkz.org>
2014-10-22 09:54:12 +0400

    2.1.0-beta9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/elastisch "2.1.0-beta9-SNAPSHOT"
+(defproject clojurewerkz/elastisch "2.1.0-beta9"
   :url "http://clojureelasticsearch.info"
   :description "Minimalistic fully featured well documented Clojure ElasticSearch client"
   :license {:name "Eclipse Public License"}
