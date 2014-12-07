b5b14de8b36287d4fec182d2a67d639cb545d0d7
Michael Klishin <michael@clojurewerkz.org>
2014-12-07 15:29:49 +0300

    APL v2.0 headers

--- src/clojurewerkz/elastisch/rest.clj
+++ src/clojurewerkz/elastisch/rest.clj
@@ -1,11 +1,16 @@
-;; Copyright (c) 2011-2014 Michael S. Klishin, Alex Petrov, and the ClojureWerkz Team
+;; Copyright 2011-2014 Michael S. Klishin, Alex Petrov, and the ClojureWerkz Team
 ;;
-;; The use and distribution terms for this software are covered by the
-;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
-;; which can be found in the file epl-v10.html at the root of this distribution.
-;; By using this software in any fashion, you are agreeing to be bound by
-;; the terms of this license.
-;; You must not remove this notice, or any other, from this software.
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;     http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
 
 (ns clojurewerkz.elastisch.rest
   (:refer-clojure :exclude [get])
