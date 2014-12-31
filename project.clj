7dd812b850548f29bcba064fa461479c8cf74bb9
Michael Klishin <michael@clojurewerkz.org>
2014-12-31 18:00:50 +0300

    Add Eastwood plugin dependency

--- project.clj
+++ project.clj
@@ -11,7 +11,8 @@
   :min-lein-version "2.5.0"
   :profiles     {:dev {:resource-paths ["test/resources"]
                        :dependencies [[clj-time "0.8.0" :exclusions [org.clojure/clojure]]]
-                       :plugins [[codox "0.8.10"]]
+                       :plugins [[codox           "0.8.10"]
+                                 [jonase/eastwood "0.2.1"]]
                        :codox {:sources ["src"]
                                :output-dir "doc/api"}}
                  ;; this version of clj-http depends on HTTPCore 4.2.x which
