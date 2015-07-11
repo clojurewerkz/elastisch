e32e60c7f0428e0c9e466296992c433c7e336f04
Michael Klishin <michael@clojurewerkz.org>
2015-07-12 01:14:31 +0300

    Clojure master is now 1.8.0-master-SNAPSHOT

--- project.clj
+++ project.clj
@@ -20,7 +20,7 @@
                  ;; so we test for compatibility with it. MK.
                  :cljhttp076 {:dependencies [[clj-http "0.7.6"]]}
                  :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta3"]]}
-                 :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
+                 :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}}
   :aliases      {"all" ["with-profile" "dev:dev,1.7:dev,cljhttp076"]}
   :repositories {"sonatype"         {:url "http://oss.sonatype.org/content/repositories/releases"
                                      :snapshots false
