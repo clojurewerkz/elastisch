574938bae2c294d4ef44db5c78d9c31f9c43469f
Michael Klishin <michael@clojurewerkz.org>
2015-12-08 12:23:51 +0300

    We depend on 1.7.0 now, no need for a separate profile

--- project.clj
+++ project.clj
@@ -19,10 +19,9 @@
                  ;; some projects (e.g. using Spring's RestTemplate) can rely on,
                  ;; so we test for compatibility with it. MK.
                  :cljhttp076 {:dependencies [[clj-http "0.7.6"]]}
-                 :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
                  :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC3"]]}
                  :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}}
-  :aliases      {"all" ["with-profile" "dev:dev,1.7:dev,cljhttp076"]}
+  :aliases      {"all" ["with-profile" "dev:dev,1.8:dev,cljhttp076"]}
   :repositories {"sonatype"         {:url "http://oss.sonatype.org/content/repositories/releases"
                                      :snapshots false
                                      :releases {:checksum :fail :update :always}}
