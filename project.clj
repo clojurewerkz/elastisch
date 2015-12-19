d5e2f9b26939314ae120ffb16e981f11a618ae0b
Michael Klishin <michael@clojurewerkz.org>
2015-12-19 11:46:14 +0300

    Clojure 1.8 is at RC4

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
                  ;; some projects (e.g. using Spring's RestTemplate) can rely on,
                  ;; so we test for compatibility with it. MK.
                  :cljhttp076 {:dependencies [[clj-http "0.7.6"]]}
-                 :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC3"]]}
+                 :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC4"]]}
                  :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}}
   :aliases      {"all" ["with-profile" "dev:dev,1.8:dev,cljhttp076"]}
   :repositories {"sonatype"         {:url "http://oss.sonatype.org/content/repositories/releases"
