e5b36fa1a93d2bc2222c11f1add24a5c30018bfe
Timo Sulg <timgluz@gmail.com>
2016-04-11 18:29:37 +0200

    fix failing testcase

--- test/clojurewerkz/elastisch/rest_api/update_test.clj
+++ test/clojurewerkz/elastisch/rest_api/update_test.clj
@@ -56,9 +56,8 @@
                (get-in (doc/get conn index-name index-type id) [:_source :biography])))
         ;; Can't perform a write when we pass the wrong version
         (let [e (:error (doc/put conn index-name index-type id (assoc fx/person-jack :biography "brilliant3") {:version original-version}))]
-          (is (.startsWith
-               e
-               "VersionConflictEngineException")))
+          (is (:type e "version_conflict_engine_exception")))
+
         ;; Still should have the new data
         (is (= "brilliant2" (get-in (doc/get conn index-name index-type id) [:_source :biography]))))))
 
