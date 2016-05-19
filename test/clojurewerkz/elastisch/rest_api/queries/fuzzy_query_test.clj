b3ea0a7efe57c133b935af9c50130050cd0d8393
Josh Tilles <josh@signafire.com>
2016-05-19 19:47:58 -0400

    Uncomment & fix fuzzy-query-with-date-fields test.
    Fixing the test involved:
    - correcting the name of the field (from `"last-edit.date"` to
      `"latest-edit.date"`)
    - correcting the ID of the returned document
      - `prepopulate-articles-index` puts the `article-on-elasticsearch`
        document —which is the one that is expected to match the query—
        under the ID “1”, not the ID “4”.

--- test/clojurewerkz/elastisch/rest_api/queries/fuzzy_query_test.clj
+++ test/clojurewerkz/elastisch/rest_api/queries/fuzzy_query_test.clj
@@ -38,12 +38,11 @@
       (is (= 1 (total-hits response)))
       (is (= #{"4"} (ids-from response)))))
 
-  ;; TODO: ES 0.19.2 spills 500s complaining about java.lang.IllegalArgumentException: minimumSimilarity >= 1
-  #_ (deftest ^{:rest true :query true} test-basic-fuzzy-query-with-date-fields
-       (let [index-name   "articles"
-             mapping-type "article"
-             response     (doc/search conn index-name mapping-type :query (q/fuzzy "last-edit.date" {:value "2012-03-25T12:00:00" :min_similarity "1d"}))
-             hits         (hits-from response)]
-         (is (any-hits? response))
-         (is (= 1 (total-hits response)))
-         (is (= #{"4"} (ids-from response))))))
+  (deftest ^{:rest true :query true} test-basic-fuzzy-query-with-date-fields
+    (let [index-name   "articles"
+          mapping-type "article"
+          response     (doc/search conn index-name mapping-type :query (q/fuzzy "latest-edit.date" {:value "2012-03-25T12:00:00" :min_similarity "1d"}))
+          hits         (hits-from response)]
+      (is (any-hits? response))
+      (is (= 1 (total-hits response)))
+      (is (= #{"1"} (ids-from response))))))
