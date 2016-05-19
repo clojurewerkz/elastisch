4401909537fb5a9dac083c90c74c7ddfba7abc2c
Josh Tilles <josh@signafire.com>
2016-05-19 18:03:23 -0400

    Fix (and uncomment) span-query tests.
    Here are some REPL interactions that illustrate some of the analyzer
    differences:
    
    ```clojure
    (require '[clojurewerkz.elastisch.rest :as es-rest])
    ;= nil
    (def conn (es-rest/connect))
    ;= #'user/conn
    (require '[clojurewerkz.elastisch.rest.document :as es-rest.doc])
    ;= nil
    (es-rest.doc/analyze conn "eating" :analyzer "standard")
    ;= {:tokens [{:token "eating", :start_offset 0, :end_offset 6, :type "<ALPHANUM>", :position 1}]}
    (es-rest.doc/analyze conn "eating" :analyzer "snowball")
    ;= {:tokens [{:token "eat", :start_offset 0, :end_offset 6, :type "<ALPHANUM>", :position 1}]}
    (es-rest.doc/analyze conn "documents" :analyzer "standard")
    ;= {:tokens [{:token "documents", :start_offset 0, :end_offset 9, :type "<ALPHANUM>", :position 1}]}
    (es-rest.doc/analyze conn "documents" :analyzer "snowball")
    ;= {:tokens [{:token "document", :start_offset 0, :end_offset 9, :type "<ALPHANUM>", :position 1}]}
    ```

--- test/clojurewerkz/elastisch/rest_api/queries/span_query_test.clj
+++ test/clojurewerkz/elastisch/rest_api/queries/span_query_test.clj
@@ -17,23 +17,27 @@
             [clojure.test :refer :all]))
 
 
-(use-fixtures :each fx/reset-indexes fx/prepopulate-articles-index)
+(use-fixtures :each fx/reset-indexes fx/prepopulate-articles-index fx/prepopulate-people-index)
 
-;; TODO: these return no results and ES test suite does not contain any meaningful tests for span queries.
-;;       Lucene test suite does have SpanFirstQuery tests but they use custom matchers we need to figure out
-;;       first. MK.
-#_ (let [conn (rest/connect)]
-     (deftest ^{:rest true :query true} test-span-first-query
-       (let [response (doc/search conn "people" "person" :query (q/span-first :match {:span_term {:biography "eating"}} :end 5))
-             hits     (hits-from response)]
-         (is (any-hits? response))
-         (is (= 1 (total-hits response)))
-         (is (= #{"1"} (set (map :_id hits))))))
+(let [conn (rest/connect)]
+  (deftest ^{:rest true :query true} test-span-first-query
+    ;; Finding the document that contains the word “eating” (i.e.,
+    ;; [[clojurewerkz.elastisch.fixtures/person-jack]]) requires searching for
+    ;; the word “eat” —not “eating”— because the `biography` field is indexed
+    ;; with the `snowball` analyzer.
+    (let [response (doc/search conn "people" "person" :query (q/span-first :match {:span_term {:biography "eat"}} :end 5))
+          hits     (hits-from response)]
+      (is (any-hits? response))
+      (is (= 1 (total-hits response)))
+      (is (= #{"1"} (set (map :_id hits))))))
 
-     (deftest ^{:rest true :query true} test-span-near-query
-       (let [response (doc/search conn "articles" "article" :query (q/span-near :clauses [{:span_term {:summary "search"}}
-                                                                                          {:span_term {:summary "documents"}}] :slop 5 :in_order true))
-             hits     (hits-from response)]
-         (is (any-hits? response))
-         (is (= 1 (total-hits response)))
-         (is (= #{"1"} (set (map :_id hits)))))))
+  (deftest ^{:rest true :query true} test-span-near-query
+    ;; Similarly to the previous test, we search for “document” instead of
+    ;; “documents” (which is what [[clojurewerkz.elastisch.fixtures/article-on-elasticsearch]])
+    ;; *actually* contains).
+    (let [response (doc/search conn "articles" "article" :query (q/span-near :clauses [{:span_term {:summary "search"}}
+                                                                                       {:span_term {:summary "document"}}] :slop 5 :in_order true))
+          hits (hits-from response)]
+      (is (any-hits? response))
+      (is (= 1 (total-hits response)))
+      (is (= #{"1"} (set (map :_id hits)))))))
