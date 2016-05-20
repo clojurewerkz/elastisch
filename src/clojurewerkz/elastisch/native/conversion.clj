ece8b996ea86497322c20f89601d2952942e7655
Josh Tilles <josh@signafire.com>
2016-05-19 20:23:14 -0400

    Simplify `c.e.n.conversion/range-bucket->map`.
    As of Elasticsearch 1.5, the methods `Range#getFromAsString` and
    `Range#getToAsString` are available.
    
    For more information, see:
    - <https://github.com/elastic/elasticsearch/commit/2b47e546ef64a6cf3dd84b5413e422cdd99b27bd>
    - <https://github.com/elastic/elasticsearch/issues/9003>

--- src/clojurewerkz/elastisch/native/conversion.clj
+++ src/clojurewerkz/elastisch/native/conversion.clj
@@ -1088,9 +1088,9 @@
   [^Range$Bucket b]
   (merge-sub-aggregations
    {:doc_count (.getDocCount b)
-    :from_as_string (String/valueOf ^{:tag "long"} (.. b getFrom longValue))
+    :from_as_string (.getFromAsString b)
     :from (.. b getFrom longValue)
-    :to_as_string (String/valueOf ^{:tag "long"} (.. b getTo longValue))
+    :to_as_string (.getToAsString b)
     :to (.. b getTo longValue)}
    b))
 
