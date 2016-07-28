25b77897f15c5353567e362ae93c4303205792e9
Josh Tilles <josh@signafire.com>
2016-07-28 02:11:51 -0400

    tmp! sync `indices.type_exists` docstring between rest & native

--- src/clojurewerkz/elastisch/native/index.clj
+++ src/clojurewerkz/elastisch/native/index.clj
@@ -91,7 +91,9 @@ The value should be an integer, the epoch time in milliseconds."
 
 
 (defn type-exists?
-  "Returns `true` if a type/types exists in an index/indices"
+  "Used to check if a type/types exists in an index/indices.
+
+  API Reference: <http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/indices-types-exists.html>"
   [^Client conn ^String index-name type-name]
   (let [ft                        (es/admin-types-exists conn (cnv/->types-exists-request index-name type-name))
         ^TypesExistsResponse res (.actionGet ft)]
