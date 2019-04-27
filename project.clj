36bf4a11163e1f0cfb9e882bb70c946fd1ca2d22
Michael Klishin <michael@clojurewerkz.org>
2019-04-27 21:38:19 +0300

    Back to dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/elastisch "3.0.1"
+(defproject clojurewerkz/elastisch "3.0.2-SNAPSHOT"
   :url "http://clojureelasticsearch.info"
   :description "Minimalistic fully featured well documented Clojure Elasticsearch client"
   :license {:name "Eclipse Public License"}
